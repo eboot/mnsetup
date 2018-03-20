d9b1e97706d9875265384e6e6d73a9ccadd2be0d
eboot <blogarticles73@gmail.com>
2018-03-20 07:30:22 +0700

    first commot

---
+++ Stake/stake_setup.sh
@@ -0,0 +1,101 @@
+#/bin/bash
+
+cd ~
+echo "* Ubuntu 16.04 is the recommended opearting system for this install.      *"
+echo "*                     STAKE COIN MN setup                                 *"
+echo "****************************************************************************"
+echo && echo && echo
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "!                                                 !"
+echo "! Make sure you double check before hitting enter !"
+echo "!                                                 !"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo && echo && echo
+
+echo "Do you want to install all needed dependencies include Stake wallet ?  (no if you did it before)? [y/n]"
+read DOSETUP
+
+if [[ $DOSETUP =~ "y" ]] ; then
+  sudo apt-get update
+  sudo apt-get -y upgrade
+  sudo apt-get -y dist-upgrade
+  sudo apt-get install -y nano htop git
+  sudo apt-get install -y software-properties-common
+  sudo apt-get install -y build-essential libtool autotools-dev pkg-config libssl-dev
+  sudo apt-get install -y libboost-all-dev
+  sudo apt-get install -y libevent-dev
+  sudo apt-get install -y libminiupnpc-dev
+  sudo apt-get install -y autoconf
+  sudo apt-get install -y automake unzip
+  sudo add-apt-repository  -y  ppa:bitcoin/bitcoin
+  sudo apt-get update
+  sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
+
+  cd /var
+  sudo touch swap.img
+  sudo chmod 600 swap.img
+  sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
+  sudo mkswap /var/swap.img
+  sudo swapon /var/swap.img
+  sudo free
+  sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
+  cd
+
+  mkdir -p ~/bin
+  echo 'export PATH=~/bin:$PATH' > ~/.bash_aliases
+  source ~/.bashrc
+
+echo ""
+echo "Download And setup stake wallet from github"
+
+git clone https://github.com/StakeDeveloper/stake
+sudo cd stake/src
+make -f makefile.unix
+sudo strip staked
+sudo cp staked /usr/local/bin
+
+fi
+
+echo ""
+echo "Configure your masternodes now!"
+echo "Type the IP of this server, followed by [ENTER]:"
+read IP
+
+echo ""
+echo "Enter masternode private key  (masternode genkey) for node $ALIAS"
+read PRIVKEY
+
+CONF_DIR=~/.Stake/
+CONF_FILE=Stake.conf
+PORT=33234
+
+mkdir -p $CONF_DIR
+echo "rpcuser=user"`shuf -i 100000-10000000 -n 1` >> $CONF_DIR/$CONF_FILE
+echo "rpcpassword=pass"`shuf -i 100000-10000000 -n 1` >> $CONF_DIR/$CONF_FILE
+echo "rpcallowip=127.0.0.1" >> $CONF_DIR/$CONF_FILE
+echo "listen=1" >> $CONF_DIR/$CONF_FILE
+echo "server=1" >> $CONF_DIR/$CONF_FILE
+echo "daemon=1" >> $CONF_DIR/$CONF_FILE
+echo "logtimestamps=1" >> $CONF_DIR/$CONF_FILE
+echo "maxconnections=256" >> $CONF_DIR/$CONF_FILE
+echo "masternode=1" >> $CONF_DIR/$CONF_FILE
+echo "" >> $CONF_DIR/$CONF_FILE
+
+echo "addnode=109.195.179.12:49815" >> $CONF_DIR/$CONF_FILE
+echo "addnode=13.124.112.170:39726" >> $CONF_DIR/$CONF_FILE
+echo "addnode=133.203.108.63:63307" >> $CONF_DIR/$CONF_FILE
+echo "addnode=133.218.201.36:64729" >> $CONF_DIR/$CONF_FILE
+echo "addnode=149.56.103.212:33234" >> $CONF_DIR/$CONF_FILE
+echo "addnode=149.56.128.71:58120" >> $CONF_DIR/
+
+echo "" >> $CONF_DIR/$CONF_FILE
+echo "port=$PORT" >> $CONF_DIR/$CONF_FILE
+echo "masternodeaddr=$IP:$PORT" >> $CONF_DIR/$CONF_FILE
+echo "masternodeprivkey=$PRIVKEY" >> $CONF_DIR/$CONF_FILE
+
+staked -daemon
+cd ~
+echo "wait block sync"
+echo "check using (staked getinfo)"
+echo "if wallet success sync all block"
+echo "use: staked masternode start and staked masternode status"
